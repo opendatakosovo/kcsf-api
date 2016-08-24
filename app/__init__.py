932f0401ea50f6cfead37b2f2fae9df2f5204c6b
Vullkan92 <vullkan.halili@riinvest.net>
2016-08-24 18:48:00 +0200

    configuration changes

--- app/__init__.py
+++ app/__init__.py
@@ -88,8 +88,6 @@ def configure_logging(app):
 def init_modules(app):
 
     # Import blueprint modules
-    from app.mod_main.views import mod_main
     from app.mod_api.views import mod_api
 
-    app.register_blueprint(mod_main)
     app.register_blueprint(mod_api)
