e9957896c639765b9bcc412e5bb9d5dcf6f03ae4
Arianit Hetemi <nitihetemi@gmail.com>
2018-05-08 17:23:38 +0200

    Update deploy.sh

--- jenkinscd/deploy.sh
+++ jenkinscd/deploy.sh
@@ -3,8 +3,6 @@ su jenkins
 ssh jenkins@46.101.135.45 <<EOF
  cd /var/www/kcsf-api
  sudo su
- eval `ssh-agent -s`
- ssh-add /home/jenkins/.ssh/id_rsa
  git pull
  rm -rf venv
  service apache2 restart
