b42f3363e05f1fc19da1de12d639326ced95eaf9
Arianit Hetemi <nitihetemi@gmail.com>
2018-08-23 14:18:13 +0200

    New structure of data

--- import.py
+++ import.py
@@ -11,7 +11,7 @@ def run_importer():
     # This is to expand the app's context(scope),
     # so that we can use mongo configurations for the importer too.
     with app.app_context():
-      DataImporter().run()
+      DataImporter().run('2017')
 
 if __name__ == '__main__':
     run_importer()
